4e1472b5f16006964b652ff26270f292a7e30a40
Ryutaro-A <ryu1104.as@gmail.com>
2023-12-10 00:08:04 +0900

    bavber

--- my-gatsby-site/src/pages/header.js
+++ my-gatsby-site/src/pages/header.js
@@ -1,50 +1,18 @@
-// components/header.js
-
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import { Container, Navbar, Nav } from 'react-bootstrap' // ←追記
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Header = ({ siteTitle }) => (
-// ↓ここから
-    // <header className="bg-light">
-    //     <Container>
-    //     <Navbar expand="md" variant="light">
-    //         <Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
-    //         <Navbar.Toggle aria-controls="navbarResponsive" />
-    //         <Navbar.Collapse id="navbarResponsive">
-    //         <Nav as="ul" className="ml-auto">
-    //             <Nav.Item as="li">
-    //             <Link to="/publications" className="nav-link" activeClassName="active">Publications</Link>
-    //             </Nav.Item>
-    //             <Nav.Item as="li">
-    //             <Link to="/page-3" className="nav-link" activeClassName="active">Page3</Link>
-    //             </Nav.Item>
-    //         </Nav>
-    //         </Navbar.Collapse>
-    //     </Navbar>
-    //     </Container>
-    // </header>
+const Header = () => {
     <Navbar bg="light" data-bs-theme="light">
         <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar.Brand href="/index">Ryutaro Asahara</Navbar.Brand>
             <Nav className="justify-content-between">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="/index">Home</Nav.Link>
+                <Nav.Link href="/publications">Publications</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
         </Container>
     </Navbar>
-    // ↑ここまで
-    )
-
-    Header.propTypes = {
-    siteTitle: PropTypes.string,
-    }
-
-    Header.defaultProps = {
-    siteTitle: ``,
 }
 
 export default Header
